0c6fffb0cd4631e6bb8c9c9656908150de89d6e5
Paulo Köch <pkoch@frctls.com>
2022-08-08 14:52:19 +0100

    Better docs, message, and accessors

--- contracts/2_Voting.sol
+++ contracts/2_Voting.sol
@@ -3,14 +3,14 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import {FractalRegistry} from "./1_FractalRegistry.sol";
 
-/// @title Simplified vote contract
-/// @notice Example usage of Fractal's DID Registry to enable "One Person <=> One Vote".
+/// @title Simplified, non-secret, voting contract
+/// @notice Toy example of Fractal's DID Registry usage to enable "One Person <=> One Vote".
 contract Voting {
     FractalRegistry fractalRegistry;
 
-    uint8 numOptions;
-    mapping(uint8 => uint8) votes;
-    mapping(bytes32 => bool) voters;
+    uint8 public numOptions;
+    mapping(uint8 => uint8) public votes;
+    mapping(bytes32 => bool) public voters;
 
     constructor(uint8 _numOptions, address _fractalRegistryAddress) {
         numOptions = _numOptions;
@@ -20,15 +20,16 @@ contract Voting {
     /// @notice Cast a vote.
     /// @param option The option to vote for.
     function vote(uint8 option) external {
-        require(option < numOptions, "Must vote for a valid option");
+        require(option < numOptions, "Invalid option: `option` must be lower than `numOptions`");
 
         bytes32 fractalId = fractalRegistry.getFractalId(msg.sender);
-        require(!voters[fractalId], "Can't vote twice");
+        require(!voters[fractalId], "Invalid option: same person can't vote twice.");
 
         voters[fractalId] = true;
         votes[option] += 1;
     }
 
+    /// @notice Get the current count for each option.
     function currentTally() external view returns(uint8[] memory) {
       uint8[] memory votes_ = new uint8[](numOptions);
       for(uint8 i = 0; i < numOptions; i++) {
